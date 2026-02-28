8b8176557f5fc7f7a020e8c3ba273d06b3851236
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-02-27 19:42:25 -0500

    Feat: get one user by ID endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,16 +1,34 @@
 const userService = require('../services/userService.js');
 
+async function showUser(req, res) {
+  const { userId } = req.body;
+
+  try {
+    const result = await userService.show(userId);
+
+    res.status(200).json({
+      result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error creating user",
+      error: error.message,
+    });
+  }
+}
+
 async function createUser(req, res) {
   const { username, first_name, last_name, password, email } = req.body;
 
   try {
-    await userService.create(
+    const result = await userService.create(
       username,
       first_name,
       last_name,
       password,
       email
     );
+
     res.status(201).json({
       message: "User created successfully",
       userId: result.insertId,
--- server/services/userService.js
+++ server/services/userService.js
@@ -1,16 +1,30 @@
 const connection = require('../db.js');
 const bcrypt = require('bcryptjs');
 
+async function show(userId) {
+  const [result] = await connection.promise().query(
+    `SELECT *
+    FROM users
+    WHERE id = (?);`,
+    [userId]
+  );
+
+  return result;
+}
+
 async function create(username, firstName, lastName, password, email) {
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  await connection.promise().query(
+  const [result] = await connection.promise().query(
     `INSERT INTO users (username, first_name, last_name, password, email)
     VALUES (?, ?, ?, ?, ?);`,
     [username, firstName, lastName, hashedPassword, email]
   );
+
+  return result;
 }
 
 module.exports = {
+  show,
   create
 };
