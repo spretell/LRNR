ef75aa8488a3bd3958c4c8e08b8fd85fbe06aafa
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-02-27 20:22:01 -0500

    Feat: update user function updates streak, level, and experience points

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { showUser, createUser } = require('../controllers/userController.js');
+const { showUser, createUser, updateUser } = require('../controllers/userController.js');
 
 const router = express.Router();
 
 router.get('/:id', showUser);
 router.post('/', createUser);
+router.post('/:id', updateUser)
 
 module.exports = router;
