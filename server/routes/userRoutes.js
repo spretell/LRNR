5baa0507f76f64a7e8732d9517c9413dda0c1939
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-02-28 10:21:54 -0500

    Feat: break up update and streak update into two functions for a more RESTful architecture

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { showUser, createUser, updateUser } = require('../controllers/userController.js');
+const { showUser, createUser, updateUser, updateUserStreak } = require('../controllers/userController.js');
 
 const router = express.Router();
 
 router.get('/:id', showUser);
 router.post('/', createUser);
-router.post('/:id', updateUser)
+router.post('/:id', updateUser);
+router.post('/:id/streak', updateUserStreak);
 
 module.exports = router;
