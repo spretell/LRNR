43b87897927788906497859138726eb44246b9c9
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-03 11:47:02 -0500

    Feat: add show action to the quiz controller and service to return all platinum quizzes for a user

--- server/controllers/quizController.js
+++ server/controllers/quizController.js
@@ -4,6 +4,7 @@
 
 // import the OpenAI service function to generate quizzes
 const { generateOpenAIQuiz } = require("../services/openaiService");
+const quizService = require('../services/quizService');
 
 // controller function for creating a quiz
 async function createQuiz(req, res) {
@@ -27,5 +28,46 @@ async function createQuiz(req, res) {
   }
 }
 
+async function showQuizzes(req, res) {
+  const userId = req.params.id;
+
+  try {
+    const result = await quizService.show(userId);
+
+    res.status(200).json({
+      data: result,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Server error',
+      error: error.message,
+    });
+  }
+}
+
+async function saveQuiz(req, res) {
+  const userId = req.params.id;
+  const { title, difficulty } = req.body;
+
+  try {
+    const result = await quizService.create(title, difficulty, userId);
+
+    const userQuizzes = await quizService.show(userId);
+
+    res.status(201).json({
+      data: userQuizzes,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      message: 'Server error',
+      error: error.message,
+    });
+  }
+}
+
 // export the controller functions so they can be used in the routes
-module.exports = { createQuiz };
+module.exports = {
+  createQuiz,
+  showQuizzes,
+  saveQuiz,
+};
---
+++ server/services/quizService.js
@@ -0,0 +1,27 @@
+const connection = require('../db.js');
+
+async function show(userId) {
+  const [result] = await connection.promise().query(
+    `SELECT *
+    FROM platinum_quizzes
+    WHERE user_id = ?`,
+    [userId]
+  );
+
+  return result;
+};
+
+async function create(title, difficulty, userId) {
+  let [result] = await connection.promise().query(
+    `INSERT INTO platinum_quizzes (quiz_name, quiz_level, user_id)
+    VALUES (?, ?, ?)`,
+    [title, difficulty, userId]
+  );
+
+  return result;
+};
+
+module.exports = {
+  show,
+  create,
+};
