5389428b373d52f40785422ec8952cae71a7492b
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-03 11:51:28 -0500

    Feat: add error handling to the quiz controller for missing fields and unable to update database

--- server/controllers/quizController.js
+++ server/controllers/quizController.js
@@ -49,8 +49,20 @@ async function saveQuiz(req, res) {
   const userId = req.params.id;
   const { title, difficulty } = req.body;
 
+  if (!title || !difficulty) {
+    return res.status(422).json({
+      message: 'All fields are required',
+    });
+  }
+
   try {
     const result = await quizService.create(title, difficulty, userId);
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({
+        message: 'Unable to save to the database',
+      });
+    }
 
     const userQuizzes = await quizService.show(userId);
 
