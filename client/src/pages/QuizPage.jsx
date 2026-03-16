3437b4fb2878bce49c1695fcdcfe62626cb2853c
Stephanie Pretell <wephiie@unknown8ed07a7162e6.attlocal.net>
2026-03-16 13:11:23 -0400

    moved QuizPage.css into styles folder for consistency

--- client/src/pages/QuizPage.jsx
+++ client/src/pages/QuizPage.jsx
@@ -1,6 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
-import "./QuizPage.css";
+import "../styles/QuizPage.css";
 
 export default function QuizPage() {
   const location = useLocation();
